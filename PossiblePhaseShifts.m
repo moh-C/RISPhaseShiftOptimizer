730637337991a4ebc3801d3bd0714988d26e0695
Aaron <mohammad.chegini@hotmail.com>
2021-05-27 12:35:51 +0430

    Slight modifications to driver and PossiblePhaseShift

--- PossiblePhaseShifts.m
+++ PossiblePhaseShifts.m
@@ -1,5 +1,8 @@
 function [PhaseShifts] = PossiblePhaseShifts(bits)
 % Given a number of bits, this function returns the possible phase shifts.
+% If the RIS is capable of 1-bit, the possible phase shifts are -1, +1
+% while if the RIS is capable of having 2 bits, the possible phase shifts
+% are: -3, -1, 1, 3
     PhaseShifts = zeros(2^bits, 1);
     for i=0:2^(bits-1)-1
         PhaseShifts(2*i+1) = 2*i+1;
